67ca3333c73f1a3c6ee0b8f7e6674a332140f3a9
Nathan <nathan.daniel5719@gmail.com>
2019-04-10 23:13:58 -0400

    configured the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+        resources :items
+        resources :orders
+        resources :pizza_types
+
+        get '/menu', to: 'pizza_types#index'
+        # get 'orders/total_price', to: 'orders#total_price', :as => 'jin'
+    end
+  end
 end
