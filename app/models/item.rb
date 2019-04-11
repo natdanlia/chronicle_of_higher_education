16e49694e812e1dd1220919f93f8899fe72117e0
Nathan <nathan.daniel5719@gmail.com>
2019-04-11 13:49:53 -0400

    added validation on item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+
   belongs_to :pizza_type
   belongs_to :order
+
+
+
+  validates :pizza_type_id,  numericality: { greater_than: 0,less_than_or_equal_to: 5 }
+  validates :quantity, numericality: { greater_than: 0,less_than_or_equal_to: 50 }
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   has_many :items
   has_many :pizza_types, through: :items
+
+  # validates :pizza_type_id,  numericality: { greater_than: 0,less_than_or_equal_to: 5 }
+  # validates :quantity, numericality: { greater_than: 0,less_than_or_equal_to: 50 }
 end
